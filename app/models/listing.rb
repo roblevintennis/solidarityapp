ead19274606103a286c560239243aa738a0b646f
Rob Levin <roblevintennis@gmail.com>
2017-05-06 00:30:00 +0000

    Poor man's search done

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -9,4 +9,10 @@ class Listing < ActiveRecord::Base
     [city, state, zipcode].join(', ')
   end
   
+  def self.search(params)
+    listings = Listing.where(category_id: params[:category].to_i)
+    listings = listings.where("title like ? or description like ?", "%#{params[:search]}%", "%#{params[:search]}%") if params[:search].present?
+    listings = listings.near(params[:location], 20) if params[:location].present?
+    listings
+  end
 end
